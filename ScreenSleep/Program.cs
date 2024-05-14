5968d9dd3f3de85cb32edee8ce6330ef5d375ca0
Shannon Graybrook <sg1578@methode.global>
2024-05-14 13:26:16 -0500

    initial commit

---
+++ ScreenSleep/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ScreenSleep
+{
+    internal static class Program
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(
+            IntPtr hWnd,
+            UInt32 Msg,
+            IntPtr wParam,
+            IntPtr lParam
+        );
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <see cref="https://superuser.com/questions/630784/how-to-manually-trigger-turn-off-display"/>
+        [STAThread]
+        static void Main()
+        {
+            SendMessage(
+                (IntPtr)0xffff, // HWND_BROADCAST
+                0x0112,         // WM_SYSCOMMAND
+                (IntPtr)0xf170, // SC_MONITORPOWER
+                (IntPtr)0x0002  // POWER_OFF
+            );
+        }
+    }
+}
